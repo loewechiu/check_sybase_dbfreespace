1178925e48e50fc985d864c00ab866b1dd9fbf75
Welson <20180429+loewechiu@users.noreply.github.com>
2020-10-10 17:36:08 +0800

    Update check_sybase_dbfreespace.py

--- check_sybase_dbfreespace.py
+++ check_sybase_dbfreespace.py
@@ -7,7 +7,7 @@
 # and then related sql commands and with "go" behind
 # Here we'll use python ODBC with devart ASE driver to connect
 # Prerequiste: 1. rpm -i devart-odbc-ase.x86_64.rpm
-#              2. pip3 install --proxy 10.134.254.52:8080 pyodbc
+#              2. pip3 install pyodbc
 #              3. let network team open port 4901,4902,... (/sybase/<SID>/interfaces)
 # Reference: https://www.devart.com/odbc/ase/docs/python.htm
 # Sample:  ./check_sybase_dbfreespace.py -H 1.1.1.1 -d STD -u myuser -p mypwd -P 4901
